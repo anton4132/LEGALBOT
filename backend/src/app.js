c64f54f49e614b5e22f50e26b509ce2e80899713
lozaAnton <jhon@idw.com.pe>
2025-09-12 11:56:09 -0500

    9.12

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: 'https://legalbot-kjwg.vercel.app/', credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
